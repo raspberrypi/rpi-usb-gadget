7c583e07894410cad05252e5c811d310b9e49efd
paul.oberosler <paul.oberosler@raspberrypi.com>
2025-09-17 16:46:55 +0100

    Switch from udev to motd + remove old code
    Signed-off-by: paul.oberosler <paul.oberosler@raspberrypi.com>

--- manual(legacy)/postinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "dtoverlay=dwc2,dr_mode=peripheral" >> /boot/firmware/config.txt || echo "dtoverlay=dwc2,dr_mode=peripheral" >> /boot/config.txt
-
-chmod +x /usr/sbin/configure-usb-ether-gadget-once.sh
-
-echo "- - USB Ethernet Gadget configured, please reboot for changes to take effect. - -"
--- manual(legacy)/postremove.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# undo mod loading
-sed -i 's/dtoverlay=dwc2,dr_mode=peripheral//g' /boot/firmware/config.txt || sed -i 's/dtoverlay=dwc2,dr_mode=peripheral//g' /boot/config.txt
